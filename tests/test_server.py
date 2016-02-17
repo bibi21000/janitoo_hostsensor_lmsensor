81ab6ff637505b4cb76a83c43c925b0e337b3654
bibi21000 <bibi21000@gmail.com>
2016-02-17 13:52:49 +0100

    Updates tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -66,8 +66,7 @@ class TestHostSensorSerser(JNTTServer, JNTTServerCommon):
     def test_101_wait_for_all_nodes(self):
         self.start()
         try:
-            self.assertHeartbeatNode(hadd=HADD%(1048,0))
-            self.assertHeartbeatNode(hadd=HADD%(1048,1))
+            self.assertHeartbeatNodes(hadds=[HADD%(1048,0), HADD%(1048,1)])
             #~ self.assertHeartbeatNode(hadd=HADD%(1048,2))
             #~ self.assertHeartbeatNode(hadd=HADD%(1048,3))
             #~ self.assertHeartbeatNode(hadd=HADD%(1048,4))
@@ -86,7 +85,6 @@ class TestHostSensorSerser(JNTTServer, JNTTServerCommon):
             self.stop()
 
     def test_112_request_nodes_and_values(self):
-        self.onlyRasperryTest()
         self.start()
         try:
             self.assertHeartbeatNode()
